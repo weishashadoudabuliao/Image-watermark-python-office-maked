300297260a07ce8569475861f89433913a08a58a
weishashadoudabuliao <102843374+weishashadoudabuliao@users.noreply.github.com>
2022-05-17 09:27:52 +0800

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'designertvtocR.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(476, 289)
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(200, 250, 75, 23))
+        self.lineEdit = QLineEdit(Dialog)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(184, 40, 133, 20))
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(130, 40, 48, 16))
+        self.label_2 = QLabel(Dialog)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(130, 110, 48, 16))
+        self.lineEdit_2 = QLineEdit(Dialog)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setGeometry(QRect(190, 110, 131, 21))
+        self.label_3 = QLabel(Dialog)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(130, 180, 54, 12))
+        self.lineEdit_3 = QLineEdit(Dialog)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.lineEdit_3.setGeometry(QRect(190, 180, 131, 20))
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"\u6c34\u5370\uff08\u56fe\u7247\uff09", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"\u786e\u5b9a", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"\u56fe\u7247\u4f4d\u7f6e", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"\u6c34\u5370\u6587\u5b57", None))
+        self.label_3.setText(QCoreApplication.translate("Dialog", u"\u6c34\u5370\u989c\u8272", None))
+    # retranslateUi
+
